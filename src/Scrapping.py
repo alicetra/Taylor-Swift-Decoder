ff5af95f8a2a5bc4e5b5489fbad031109e202632
alicetra <alicetra0109@gmail.com>
2023-10-19 14:30:51 +1100

    grab song details

---
+++ src/Scrapping.py
@@ -0,0 +1,11 @@
+from lyricsgenius import Genius
+
+
+genius = Genius("access token")
+# while try loop is there so that the time run out error message will be bypassed
+while True:
+    try:
+        artist=(genius.search_artist("Taylor Swift", max_songs=10000))
+        break
+    except:
+        pass
