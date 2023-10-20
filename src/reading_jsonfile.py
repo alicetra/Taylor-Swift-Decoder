1153386e5082b319c188e03ba6a700ba2df71691
alicetra <alicetra0109@gmail.com>
2023-10-20 12:13:17 +1100

    did a count check to make sure it captured all songs

--- src/reading_jsonfile.py
+++ src/reading_jsonfile.py
@@ -1,6 +1,8 @@
 def get_titles_lyrics(data):
     # Access 'songs' key which is a list of dictionaries
     songs = data['songs']
+    # # Initialize a counter to check it is reading all songs lyrics and titles
+    # count = 0
     # Initialize an empty list to store song details
     song_details = []
     # Iterate over the json file and access 'lyrics' and 'title_with_featured' keys for each song
@@ -9,4 +11,5 @@ def get_titles_lyrics(data):
             lyrics = song['lyrics']
             title_with_featured = song['title_with_featured']
             song_details.append((title_with_featured, lyrics))
+            # count += 1
     return song_details
