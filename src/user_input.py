77f4beb2813425b504e9bc8c6c9a7d381dbc9dca
alicetra <alicetra0109@gmail.com>
2023-10-23 13:24:57 +1100

    convert input letters into upper cases

--- src/user_input.py
+++ src/user_input.py
@@ -2,6 +2,8 @@ def user_input():
         letters_to_decode = input("Please enter your friendship bracelet letters: ")
         # Converting each letter from user input to a list
         letters_to_decode_list = list(letters_to_decode)
+        # Make sure that the input will be converted to upper case so that it will match title_details format
+        letters_to_decode_list = [letter.upper() for letter in letters_to_decode_list]
         print(letters_to_decode_list)
         return letters_to_decode_list
 
