d263ec77a14e4ee6b99d684f7589e420839b88ce
alicetra <alicetra0109@gmail.com>
2023-10-24 20:45:10 +1100

    split words

--- src/text_formatting.py
+++ src/text_formatting.py
@@ -5,4 +5,6 @@ def text_formatting(text):
     text = re.sub(r"-"," ",text)
     # Remove unicode from utf-8 files by converting to ascii and then back to its original form 
     text = text.encode("ascii", "ignore").decode()
-    return text
+    text = text.upper()
+    words = text.split()
+    return text,words
