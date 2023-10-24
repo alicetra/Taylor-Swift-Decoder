61f5d02b157c31b67572bc7f49236ed2939248c7
alicetra <alicetra0109@gmail.com>
2023-10-24 20:44:56 +1100

    remove unicode by converting to ascii and then back to original form

--- src/text_formatting.py
+++ src/text_formatting.py
@@ -3,4 +3,6 @@ import re
 def text_formatting(text):
     text = re.sub(r"\(.*?\)|\[.*?\]|/|,|\"|'|(\.\.\.)|\.", "", text)
     text = re.sub(r"-"," ",text)
+    # Remove unicode from utf-8 files by converting to ascii and then back to its original form 
+    text = text.encode("ascii", "ignore").decode()
     return text
