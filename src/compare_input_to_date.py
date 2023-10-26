fe0e439ac386e382ad0260780c6600d4a447830d
alicetra <alicetra0109@gmail.com>
2023-10-26 16:25:39 +1100

    Case handle for no match found

--- src/compare_input_to_date.py
+++ src/compare_input_to_date.py
@@ -4,6 +4,7 @@ from ordered_subset import locate_ordered_lyric_subsets
 def compare_input_to_data(letters):
     printed_titles = set()
     printed_lyrics = set()
+    found_match = False
 
     # Compare input to title.
     for i in range(len(title_details)):
@@ -19,10 +20,15 @@ def compare_input_to_data(letters):
         if matches:
             words = lyrics_details[i][0].split()
             # Loop through all matches.
-            for match in matches:  
-                # Print lyrics full words and joining them into a string.
+            for match in matches: 
+                # Print lyrics full words and joining them into a string. 
                 lyrics = ' '.join(words[match[0]:match[1]])  
                 if lyrics not in printed_lyrics:  
                     print(f"\nLyrics: {lyrics}")
                     print(f"Title of Song: {title_details[i][0]}\n")
                     printed_lyrics.add(lyrics)  
+                    # Set the flag to True since a match was found.
+                    found_match = True  
+    # If no match was found after all iterations.
+    if not found_match:  
+        print("The abbreviation doesn't match any lyrics or song title")
